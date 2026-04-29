987773b7303fb3698187840d6280a6a82f3f5417
Divyansh Kataria <217927899+divyansh-kataria@users.noreply.github.com>
2026-04-29 14:38:48 +0530

    initial commit

---
+++ hypr/cfg/programs.lua
@@ -0,0 +1,26 @@
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--  PROGRAMS CONFIGURATION
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+-- Global IPC base
+ipc = "qs -c noctalia-shell ipc call"
+
+-- Core Applications
+terminal = "kitty"
+browser = "firefox"
+fileManager = "nautilus"
+music = "spotify"
+
+-- Menus & UI
+menu = ipc .. " launcher toggle"
+clipboard = ipc .. " launcher clipboard"
+emoji = ipc .. " launcher emoji"
+sessionmenu = ipc .. " sessionMenu toggle"
+systemmonitor = ipc .. " systemMonitor toggle"
+settings = ipc .. " settings toggle"
+controlcenter = ipc .. " controlCenter toggle"
+
+-- Utilities
+lock = ipc .. " lockScreen lock"
+screenshot_area = 'grim -g "$(slurp)" - | wl-copy'
+screenshot_full = "grim - | wl-copy"
