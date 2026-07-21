4c94dcc34d42bfcad061b680ca44939fc925134d
Divyansh Kataria <217927899+divyansh-kataria@users.noreply.github.com>
2026-07-21 14:26:26 +0530

    added onest font

--- scripts/gsettings.sh
+++ scripts/gsettings.sh
@@ -12,6 +12,8 @@ gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"
 gsettings set org.gnome.desktop.interface cursor-theme "capitaine-cursors-light"
 gsettings set org.gnome.desktop.interface cursor-size 24
 gsettings set org.gnome.desktop.interface gtk-enable-primary-paste true
+gsettings set org.gnome.desktop.interface font-name "Onest 11"
+gsettings set org.gnome.desktop.interface document-font-name "Onest 12"
 gsettings set org.gnome.desktop.interface monospace-font-name "Maple Mono NF 11"
 xdg-user-dirs-gtk-update
 
